c9c1d37b2dbded06daddbb16996fe40c64190461
whitewatercn <whitewatercn@outlook.com>
2024-03-26 17:34:59 +0800

    适配pip

--- rimetool/rimetool.py
+++ rimetool/rimetool.py
@@ -1,15 +1,14 @@
 import argparse
 import vcf_to_dict
 
-
-if __name__ == '__main__':
+def main():
+	# parser = argparse.ArgumentParser(description='rimetool是一个处理rime词典的工具包，详见
 	parser = argparse.ArgumentParser(description='rimetool是一个处理rime词典的工具包，详见https://github.com/whitewatercn/rimetool/')
 	parser.add_argument('-vcf', type=str, help='.vcf的文件路径')
 	# parser.add_argument('-output', type=str, help='输出文件路径，如未指出，默认在当前目录下创建一个rimedic文件夹')
 	args = parser.parse_args()
+	vcf_to_dict.main(args.vcf)
 
-# print(args.vcf)
-vcf_to_dict.main(args.vcf)
 
 if __name__ == '__main__':
 	main()
--- setup.py
+++ setup.py
@@ -5,44 +5,38 @@ setup(
     name='rimetool',  # 模块名
     version='0.0.1',  # 当前版本
     description='rime输入法相关工具',  # 简短描述
-    py_modules=["vcf_to_dict"], # 单文件模块写法
-    # ckages=find_packages(exclude=['contrib', 'docs', 'tests']),  # 多文件模块写法
-    
+    py_modules=["rimetool"], # 单文件模块写法
     
     # 以下均为可选参数
     long_description="rime输入法相关工具",# 长描述
     url='https://github.com/whitewatercn/rimetool', # 主页链接
     author='whitewatercn', # 作者名
     author_email='whitewatercn@outlook.com', # 作者邮箱
+	packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',  # 当前开发进度等级（测试版，正式版等）
 
         'Intended Audience :: Developers', # 模块适用人群
         'Topic :: Software Development :: Build Tools', # 给模块加话题标签
-        'License :: OSI Approved :: MIT License', # 模块的license
+        'License :: OSI Approved :: GPL License', # 模块的license
 
-        'Programming Language :: Python :: 2', # 模块支持的Python版本
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-		'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-		'Programming Language :: Python :: 3.9',
-		'Programming Language :: Python :: 3.10',
+        # 'Programming Language :: Python :: 2', # 模块支持的Python版本
+        # 'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
+        # 'Programming Language :: Python :: 3.6',
+		# 'Programming Language :: Python :: 3.7',
+        # 'Programming Language :: Python :: 3.8',
+		# 'Programming Language :: Python :: 3.9',
+		# 'Programming Language :: Python :: 3.10',
     ],
-    keywords='rime input method editor tool',  # 模块的关键词，使用空格分割
+    keywords=['rime',
+			  'input method editor tool',
+			  'python'],  # 模块的关键词，使用空格分割
     install_requires=['pypinyin',
 					  'argparse'], # 依赖模块
-    # extras_require={  # 分组依赖模块，可使用pip install sampleproject[dev] 安装分组内的依赖
-    #     'dev': ['check-manifest'],
-    #     'test': ['coverage'],
-    # },
-    # package_data={  # 模块所需的额外文件
-        # 'sample': ['package_data.dat'],
-    # },
-    # data_files=[('my_data', ['data/data_file'])], # 类似package_data, 但指定不在当前包目录下的文件
+    python_requires='>=3',  # 模块支持的Python版本
     entry_points={  # 新建终端命令并链接到模块函数
         'console_scripts': [
             'rimetool=rimetool:main',
@@ -50,7 +44,6 @@ setup(
         },
         project_urls={  # 项目相关的额外链接
         'Bug Reports': 'https://github.com/whitewatercn/rimetool/issues',
-        'Say Thanks!': 'http://xlog.whitewater.wang',
         'Source': 'https://github.com/whitewatercn/rimetool',
     },
 )
